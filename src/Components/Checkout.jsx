2e36332e9d427116d097f9aeb7349fe362f61c2f
Collins Mwendwa <collinsmwendwa1968@gmail.com>
2023-07-03 20:36:02 +0300

    payment success

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { apiDomain } from "../utils/utils";
+import { CartContext } from "../context/CartContext/Context";
+import { useContext } from "react";
 import CheckoutForm from "./CheckoutForm";
+
 import "./Checkout.css";
 
 
@@ -12,21 +15,24 @@ import "./Checkout.css";
 const stripePromise = loadStripe("pk_test_51NM8u2LGQyKikAGGD0WYndHP2FL57elqLC8m0CoM1mcb6unFqKQHQoYmw4LTLLco1rwKuiu4yyeaXTskABg8fB5K00ZiUtGlwS");
 
 export default function Checkout() {
+  const {cartId} = useContext(CartContext);
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch(`${apiDomain}/create-payment-intent`, {
+    fetch(`${apiDomain}/create-payment-intent/${cartId}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ items: [{ id: "books" }] }),
     })
       .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
+      .then((data) => setClientSecret(data.clientSecret))
+      .catch((err) => console.log(err));
+
   }, []);
 
   const appearance = {
-    theme: 'stripe',
+    theme: 'night',
   };
   const options = {
     clientSecret,
